e30fc09caa09e7fadd2eda5b7d01a406a3a9d37f
Deleted-0970 <derekliuj@gmail.com>
2023-08-01 00:24:40 -0700

    adds image for Rmd. Adds some answers to .Rmd summary and some calculations in summary.R

--- summary.R
+++ summary.R
@@ -1,7 +1,23 @@
+# Reading and storing the csv file
 # Variable to ensure line is <80 characters long
 filename <- paste0("https://github.com/melaniewalsh/Neat-Datasets/",
                   "blob/main/us-prison-jail-rates-1990.csv?raw=true")
-data <- read.csv(
-  filename,
-  stringsAsFactors = FALSE
-)
+data <- read.csv(filename, stringsAsFactors = FALSE)
+
+# loading necessary libraries
+library(dplyr)
+
+# Calculating the average jail rate per 100,000 of the latinx community in the
+# United States.
+avg_latinx_jail <- data %>%
+  filter(year == max(year, na.rm = TRUE)) %>%
+  summarize(mean_latinx_jail_pop_rate =
+              round(mean(latinx_jail_pop_rate, na.rm = TRUE), 2))
+
+# Calculating the average jail rate per 100,000 of the white community in the
+# United States.
+avg_white_jail <- data %>%
+  filter(year == max(year, na.rm = TRUE)) %>%
+  summarize(mean_white_jail_pop_rate =
+              round(mean(white_jail_pop_rate, na.rm = TRUE), 2))
+
